9564d9c7c6a4ae25ba0c06fe01c1f4c62e67afe2
Yurii Pavlenko <java69ed@gmail.com>
2026-01-07 14:54:43 +0200

    change author email 070125_1454

--- src/com/yurii/pavlenko/Task_1_App.java
+++ src/com/yurii/pavlenko/Task_1_App.java
@@ -2,6 +2,7 @@ package com.yurii.pavlenko;
 
 public class Task_1_App {
     public static void main(String[] args) {
+
         System.out.println(2025);
         System.out.println("\ud83d\ude05");
     }
